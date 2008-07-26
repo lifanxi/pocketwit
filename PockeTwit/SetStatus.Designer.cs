6cbae60757ba0585937439245f9abbca7f08a51c
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-07-26 13:18:52 +0000

    Fix issue cancelling status update. Added Direct Message. Added is.gd url shortening.

--- PockeTwit/SetStatus.Designer.cs
+++ PockeTwit/SetStatus.Designer.cs
@@ -34,6 +34,7 @@
             this.menuSubmit = new System.Windows.Forms.MenuItem();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.lblCharsLeft = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainMenu1
@@ -77,6 +78,18 @@
             this.lblCharsLeft.Size = new System.Drawing.Size(39, 20);
             this.lblCharsLeft.Text = "label1";
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular);
+            this.button1.ForeColor = System.Drawing.Color.LightGray;
+            this.button1.Location = new System.Drawing.Point(4, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Insert URL";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // SetStatus
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
@@ -84,6 +97,7 @@
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(240, 294);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblCharsLeft);
             this.Controls.Add(this.textBox1);
             this.ForeColor = System.Drawing.Color.LightGray;
@@ -102,5 +116,6 @@
         private System.Windows.Forms.Label lblCharsLeft;
         private System.Windows.Forms.MenuItem menuCancel;
         private System.Windows.Forms.MenuItem menuSubmit;
+        private System.Windows.Forms.Button button1;
     }
 }
---
+++ PockeTwit/URLForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PockeTwit
+{
+    partial class URLForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.menuCancel = new System.Windows.Forms.MenuItem();
+            this.menuOK = new System.Windows.Forms.MenuItem();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuCancel);
+            this.mainMenu1.MenuItems.Add(this.menuOK);
+            // 
+            // txtURL
+            // 
+            this.txtURL.BackColor = System.Drawing.Color.Black;
+            this.txtURL.ForeColor = System.Drawing.Color.LightGray;
+            this.txtURL.Location = new System.Drawing.Point(4, 4);
+            this.txtURL.Multiline = true;
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(233, 261);
+            this.txtURL.TabIndex = 0;
+            // 
+            // menuCancel
+            // 
+            this.menuCancel.Text = "Cancel";
+            this.menuCancel.Click += new System.EventHandler(this.menuCancel_Click);
+            // 
+            // menuOK
+            // 
+            this.menuOK.Text = "OK";
+            this.menuOK.Click += new System.EventHandler(this.menuOK_Click);
+            // 
+            // URLForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.txtURL);
+            this.Menu = this.mainMenu1;
+            this.Name = "URLForm";
+            this.Text = "Enter a URL";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.MenuItem menuCancel;
+        private System.Windows.Forms.MenuItem menuOK;
+    }
+}
