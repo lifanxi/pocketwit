4b4ec0abb2795d9fa03ea6030e60377f48017f47
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-07-28 15:20:03 +0000

    Don't make UpdateChecker static.

--- PockeTwit/SettingsHandler/SettingsForm.cs
+++ PockeTwit/SettingsHandler/SettingsForm.cs
@@ -35,10 +35,6 @@ namespace PockeTwit
         {
             ClientSettings.UserName = txtUserName.Text;
             ClientSettings.Password = txtPassword.Text;
-            if (!ClientSettings.CheckVersion && chkVersion.Checked)
-            {
-                UpdateChecker.CheckForUpdate();
-            }
             ClientSettings.CheckVersion = chkVersion.Checked;
             ClientSettings.SaveSettings();
             this.DialogResult = DialogResult.OK;
