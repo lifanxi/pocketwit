8e7f3ad97a836bbc3485957aea567201650d3d4e
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-07-29 15:54:05 +0000

    Catch exceptions connecting to twitter.

--- trunk/PockeTwit/Program.cs
+++ trunk/PockeTwit/Program.cs
@@ -21,8 +21,19 @@ namespace PockeTwit
                 SettingsForm settings = new SettingsForm();
                 if (settings.ShowDialog() == DialogResult.Cancel) { return; }
             }
-
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
             Application.Run(new TweetList());
         }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is System.Net.WebException)
+            {
+                System.Net.WebException ex = (System.Net.WebException)e.ExceptionObject;
+                MessageBox.Show("Unable to connect to twitter.\r\n Either twitter is down or the network connection has been broken.", "Error");
+                Application.Exit();
+            }
+        }
+
     }
 }
