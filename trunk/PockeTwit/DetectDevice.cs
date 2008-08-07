9eb32cbfa24b3cd5672ad83ca86d292e4b317d41
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-08-07 14:43:18 +0000

    Working on smartphone.

---
+++ trunk/PockeTwit/DetectDevice.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace PockeTwit
+{
+    class DetectDevice
+    {
+        [DllImport("CoreDLL.dll")]
+        static extern bool SystemParametersInfo(uint uiAction, uint uiParam, StringBuilder pvParam, uint unused);
+        const uint SPI_GETPLATFORMTYPE=257;
+        const int _bufferSize = 32;
+
+        const string _smartphoneTypeString = "SmartPhone";
+        const string _pocketPcTypeString = "PocketPC";
+
+        private static string GetPlatformType()
+        {
+            StringBuilder platformType = new StringBuilder(_bufferSize);
+            SystemParametersInfo(SPI_GETPLATFORMTYPE, _bufferSize, platformType, 0);
+            return platformType.ToString();
+        }
+
+        private static DeviceType _DeviceType = DeviceType.Undefined;
+        public static DeviceType DeviceType
+        {
+            get
+            {
+                if (_DeviceType == DeviceType.Undefined)
+                {
+                    string platformType = GetPlatformType();
+
+                    switch (platformType)
+                    {
+                        case _smartphoneTypeString:
+                            _DeviceType = DeviceType.Standard;
+                            break;
+                        case _pocketPcTypeString:
+                            _DeviceType = DeviceType.Professional;
+                            break;
+                    }
+                }
+                return _DeviceType;
+            }
+        }
+
+    }
+
+    [Flags]
+    enum DeviceType
+    {
+        Undefined = 0x00,
+        Professional = 0x01,
+        Standard = 0x02
+    }
+}
--- trunk/PockeTwit/SetStatus.Designer.cs
+++ trunk/PockeTwit/SetStatus.Designer.cs
@@ -31,18 +31,15 @@
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
             this.menuCancel = new System.Windows.Forms.MenuItem();
-            this.menuSubmit = new System.Windows.Forms.MenuItem();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.lblCharsLeft = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnPic = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            
             this.SuspendLayout();
             // 
             // mainMenu1
             // 
             this.mainMenu1.MenuItems.Add(this.menuCancel);
-            this.mainMenu1.MenuItems.Add(this.menuSubmit);
+            
             // 
             // menuCancel
             // 
@@ -51,8 +48,6 @@
             // 
             // menuSubmit
             // 
-            this.menuSubmit.Text = "Submit";
-            this.menuSubmit.Click += new System.EventHandler(this.menuSubmit_Click);
             // 
             // textBox1
             // 
@@ -80,35 +75,6 @@
             this.lblCharsLeft.Size = new System.Drawing.Size(39, 20);
             this.lblCharsLeft.Text = "label1";
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Black;
-            this.button1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular);
-            this.button1.ForeColor = System.Drawing.Color.LightGray;
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(64, 20);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Insert URL";
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // btnPic
-            // 
-            this.btnPic.BackColor = System.Drawing.Color.Black;
-            this.btnPic.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular);
-            this.btnPic.ForeColor = System.Drawing.Color.LightGray;
-            this.btnPic.Location = new System.Drawing.Point(73, 3);
-            this.btnPic.Name = "btnPic";
-            this.btnPic.Size = new System.Drawing.Size(90, 20);
-            this.btnPic.TabIndex = 3;
-            this.btnPic.Text = "Include Picture";
-            this.btnPic.Click += new System.EventHandler(this.btnPic_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.Filter = "Images | *.jpg;*.jpeg";
-            // 
             // SetStatus
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
@@ -116,8 +82,6 @@
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(240, 294);
-            this.Controls.Add(this.btnPic);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblCharsLeft);
             this.Controls.Add(this.textBox1);
             this.ForeColor = System.Drawing.Color.LightGray;
@@ -136,7 +100,11 @@
         private System.Windows.Forms.Label lblCharsLeft;
         private System.Windows.Forms.MenuItem menuCancel;
         private System.Windows.Forms.MenuItem menuSubmit;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuURL;
+        private System.Windows.Forms.MenuItem menuPic;
+
+        private System.Windows.Forms.Button btnURL;
         private System.Windows.Forms.Button btnPic;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
