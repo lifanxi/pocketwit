8ba35051b1bbdc7498ef8ee3e4d8d4c9f10a0bda
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-08-09 13:22:58 +0000

    Update to v.20

--- PockeTwit/UpdateChecker.cs
+++ PockeTwit/UpdateChecker.cs
@@ -11,7 +11,7 @@ namespace PockeTwit
 {
     public class UpdateChecker
     {
-        public double currentVersion = .19;
+        public double currentVersion = .20;
         private string UpdateURL = "http://pocketwit.googlecode.com/svn/LatestRelease/Release.xml";
         private string XMLResponse;
         private UpdateInfo WebVersion;
