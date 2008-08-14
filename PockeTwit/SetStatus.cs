c4cf28418ff393528eaf22b6cf7cce31f2cddc90
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-08-14 14:44:45 +0000

    Working on VGA layout. Dev build.

--- PockeTwit/SetStatus.cs
+++ PockeTwit/SetStatus.cs
@@ -187,7 +187,7 @@ namespace PockeTwit
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(66, 29);
+            this.pictureBox1.Location = new System.Drawing.Point(66, ClientSettings.TextSize + 20 );
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(25, 25);
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -195,7 +195,7 @@ namespace PockeTwit
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(35, 29);
+            this.pictureBox2.Location = new System.Drawing.Point(35, ClientSettings.TextSize + 20);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(25, 25);
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
@@ -203,7 +203,7 @@ namespace PockeTwit
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(4, 29); 
+            this.pictureBox3.Location = new System.Drawing.Point(4, ClientSettings.TextSize + 20); 
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(25, 25);
             this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
