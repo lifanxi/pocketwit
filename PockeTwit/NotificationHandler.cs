f3aa4c365fc71732260ca734bb5ae7fdab1e1720
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-08-17 18:24:57 +0000

    Fighting with notifications

--- PockeTwit/NotificationHandler.cs
+++ PockeTwit/NotificationHandler.cs
@@ -10,9 +10,11 @@ namespace PockeTwit
     class NotificationHandler
     {
         public delegate void delNotificationClicked();
-        public event delNotificationClicked NotificationClicked;
+        public event delNotificationClicked FriendsNotificationClicked;
+        public event delNotificationClicked MessagesNotificationClicked;
         private Timer t_StopVibrate = new Timer();
-        private Notification Bubbler;
+        private christec.windowsce.forms.NotificationWithSoftKeys FriendsBubbler;
+        private christec.windowsce.forms.NotificationWithSoftKeys MessagesBubbler;
         [Flags]
         private enum Options
         {
@@ -37,10 +39,38 @@ namespace PockeTwit
         {
             if (DetectDevice.DeviceType == DeviceType.Professional)
             {
-                Bubbler = new Notification();
-                Bubbler.ResponseSubmitted += new ResponseSubmittedEventHandler(Bubbler_ResponseSubmitted);
+                FriendsBubbler = new christec.windowsce.forms.NotificationWithSoftKeys();
+                FriendsBubbler.LeftSoftKey = new christec.windowsce.forms.NotificationSoftKey(christec.windowsce.forms.SoftKeyType.Dismiss, "Dismiss");
+                FriendsBubbler.RightSoftKey = new christec.windowsce.forms.NotificationSoftKey(christec.windowsce.forms.SoftKeyType.StayOpen, "Show");
+                FriendsBubbler.RightSoftKeyClick += new EventHandler(FriendsBubbler_RightSoftKeyClick);
+                FriendsBubbler.Silent = true;
+
+                MessagesBubbler = new christec.windowsce.forms.NotificationWithSoftKeys();
+                MessagesBubbler.LeftSoftKey = new christec.windowsce.forms.NotificationSoftKey(christec.windowsce.forms.SoftKeyType.Dismiss, "Dismiss");
+                MessagesBubbler.RightSoftKey = new christec.windowsce.forms.NotificationSoftKey(christec.windowsce.forms.SoftKeyType.StayOpen, "Show");
+                MessagesBubbler.RightSoftKeyClick += new EventHandler(MessagesBubbler_RightSoftKeyClick);
+                MessagesBubbler.Silent = true;
+
             }
             t_StopVibrate.Tick += new EventHandler(t_StopVibrate_Tick);
+        }
+
+        void MessagesBubbler_RightSoftKeyClick(object sender, EventArgs e)
+        {
+            MessagesBubbler.Visible = false;
+            if (MessagesNotificationClicked != null)
+            {
+                MessagesNotificationClicked();
+            }
+        }
+
+        void FriendsBubbler_RightSoftKeyClick(object sender, EventArgs e)
+        {
+            MessagesBubbler.Visible = false;
+            if (FriendsNotificationClicked != null)
+            {
+                FriendsNotificationClicked();
+            }
         }
 
         void t_StopVibrate_Tick(object sender, EventArgs e)
@@ -64,6 +94,7 @@ namespace PockeTwit
 
         public void NewFriendMessages(int Count)
         {
+            LoadSettings();
             if ((Friends.Options & Options.Sound) == Options.Sound)
             {
                 Sound s = new Sound(Friends.Sound);
@@ -80,22 +111,16 @@ namespace PockeTwit
                 System.Text.StringBuilder HTMLString = new StringBuilder();
                 HTMLString.Append("<html><body>");
                 HTMLString.Append(Count.ToString() + " new friend updates are available!");
-                HTMLString.Append("<form method=\'GET\' action=notify>");
-                HTMLString.Append("<p align=\"right\">");
-                HTMLString.Append("<input type=button name='Show' value='Show'>");
-                HTMLString.Append("</p>");
-                HTMLString.Append("</form>");
                 HTMLString.Append("</body></html>");
-                Bubbler.Text = HTMLString.ToString();
-
-                Bubbler.Caption = "PockeTwit";
-                Bubbler.Visible = true;
-
+                FriendsBubbler.Text = HTMLString.ToString();
+                FriendsBubbler.Caption = "PockeTwit";
+                FriendsBubbler.Visible = true;
             }
         }
 
         public void NewMessages(int Count)
         {
+            LoadSettings();
             if ((Messages.Options & Options.Sound) == Options.Sound)
             {
                 Sound s = new Sound(Messages.Sound);
@@ -113,27 +138,13 @@ namespace PockeTwit
                 HTMLString.Append("<html><body>");
                 HTMLString.Append(Count.ToString() + " new messages are available!");
                 HTMLString.Append("<form method=\'GET\' action=notify>");
-                HTMLString.Append("<p align=\"right\">");
-                HTMLString.Append("<input type=button name='Show' value='Show'>");
-                HTMLString.Append("</p>");
-                HTMLString.Append("</form>");
                 HTMLString.Append("</body></html>");
-                Bubbler.Text = HTMLString.ToString();
-                
-                Bubbler.Caption = "PockeTwit";
-                Bubbler.Visible = true;
-                
+                MessagesBubbler.Text = HTMLString.ToString();
+                MessagesBubbler.Caption = "PockeTwit";
+                MessagesBubbler.Visible = true;
             }
         }
 
-        void Bubbler_ResponseSubmitted(object sender, ResponseSubmittedEventArgs e)
-        {
-            Bubbler.Visible = false;
-            if (NotificationClicked != null)
-            {
-                NotificationClicked();
-            }
-        }
 
         #region VibrateCode
         [System.Runtime.InteropServices.DllImport("aygshell.dll")]
