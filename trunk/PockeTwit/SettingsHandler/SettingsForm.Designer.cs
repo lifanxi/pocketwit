1d2d464943ec371b59f2fc911a2b128f46ec6db8
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-08-23 18:09:53 +0000

    Make it fit in smartphone.

--- trunk/PockeTwit/SettingsHandler/SettingsForm.Designer.cs
+++ trunk/PockeTwit/SettingsHandler/SettingsForm.Designer.cs
@@ -13,7 +13,7 @@ namespace PockeTwit
         /// </summary>
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTweets;
         private System.Windows.Forms.MainMenu mainMenu1;
         private System.Windows.Forms.MenuItem menuAccept;
         private System.Windows.Forms.MenuItem menuCancel;
@@ -58,9 +58,8 @@ namespace PockeTwit
             this.chkBeep = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
             this.txtMaxTweets = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lblTweets = new System.Windows.Forms.Label();
             this.chkReplyImages = new System.Windows.Forms.CheckBox();
-            this.lblError = new System.Windows.Forms.Label();
             this.lnkAccounts = new System.Windows.Forms.LinkLabel();
             this.chkTimestamps = new System.Windows.Forms.CheckBox();
             this.lnkClearAvatars = new System.Windows.Forms.LinkLabel();
@@ -85,7 +84,7 @@ namespace PockeTwit
             // chkVersion
             // 
             this.chkVersion.ForeColor = System.Drawing.Color.LightGray;
-            this.chkVersion.Location = new System.Drawing.Point(1, 128);
+            this.chkVersion.Location = new System.Drawing.Point(1, 108);
             this.chkVersion.Name = "chkVersion";
             this.chkVersion.Size = new System.Drawing.Size(235, 20);
             this.chkVersion.TabIndex = 4;
@@ -94,7 +93,7 @@ namespace PockeTwit
             // chkBeep
             // 
             this.chkBeep.ForeColor = System.Drawing.Color.LightGray;
-            this.chkBeep.Location = new System.Drawing.Point(1, 50);
+            this.chkBeep.Location = new System.Drawing.Point(1, 30);
             this.chkBeep.Name = "chkBeep";
             this.chkBeep.Size = new System.Drawing.Size(235, 20);
             this.chkBeep.TabIndex = 1;
@@ -103,7 +102,7 @@ namespace PockeTwit
             // label4
             // 
             this.label4.ForeColor = System.Drawing.Color.LightGray;
-            this.label4.Location = new System.Drawing.Point(4, 24);
+            this.label4.Location = new System.Drawing.Point(4, 4);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(83, 20);
             this.label4.Text = "Max Length:";
@@ -113,39 +112,28 @@ namespace PockeTwit
             this.txtMaxTweets.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.txtMaxTweets.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.txtMaxTweets.Location = new System.Drawing.Point(81, 23);
+            this.txtMaxTweets.Location = new System.Drawing.Point(81, 3);
             this.txtMaxTweets.Name = "txtMaxTweets";
             this.txtMaxTweets.Size = new System.Drawing.Size(92, 21);
             this.txtMaxTweets.TabIndex = 0;
             // 
-            // label5
+            // lblTweets
             // 
-            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label5.ForeColor = System.Drawing.Color.LightGray;
-            this.label5.Location = new System.Drawing.Point(179, 24);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(57, 20);
-            this.label5.Text = "(10-200)";
+            this.lblTweets.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTweets.ForeColor = System.Drawing.Color.LightGray;
+            this.lblTweets.Location = new System.Drawing.Point(179, 4);
+            this.lblTweets.Name = "lblTweets";
+            this.lblTweets.Size = new System.Drawing.Size(57, 20);
+            this.lblTweets.Text = "(10-200)";
             // 
             // chkReplyImages
             // 
             this.chkReplyImages.ForeColor = System.Drawing.Color.LightGray;
-            this.chkReplyImages.Location = new System.Drawing.Point(1, 76);
+            this.chkReplyImages.Location = new System.Drawing.Point(1, 56);
             this.chkReplyImages.Name = "chkReplyImages";
             this.chkReplyImages.Size = new System.Drawing.Size(235, 20);
             this.chkReplyImages.TabIndex = 2;
             this.chkReplyImages.Text = "Show reply images";
-            // 
-            // lblError
-            // 
-            this.lblError.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblError.ForeColor = System.Drawing.Color.Red;
-            this.lblError.Location = new System.Drawing.Point(4, 4);
-            this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(233, 20);
-            this.lblError.Text = "Unable to verify username and password";
-            this.lblError.Visible = false;
             // 
             // lnkAccounts
             // 
@@ -154,14 +142,14 @@ namespace PockeTwit
             this.lnkAccounts.Location = new System.Drawing.Point(127, 228);
             this.lnkAccounts.Name = "lnkAccounts";
             this.lnkAccounts.Size = new System.Drawing.Size(109, 20);
-            this.lnkAccounts.TabIndex = 5;
+            this.lnkAccounts.TabIndex = 6;
             this.lnkAccounts.Text = "Manage Accounts";
             this.lnkAccounts.Click += new System.EventHandler(this.lnkAccounts_Click);
             // 
             // chkTimestamps
             // 
             this.chkTimestamps.ForeColor = System.Drawing.Color.LightGray;
-            this.chkTimestamps.Location = new System.Drawing.Point(1, 102);
+            this.chkTimestamps.Location = new System.Drawing.Point(1, 82);
             this.chkTimestamps.Name = "chkTimestamps";
             this.chkTimestamps.Size = new System.Drawing.Size(235, 20);
             this.chkTimestamps.TabIndex = 3;
@@ -174,17 +162,17 @@ namespace PockeTwit
             this.lnkClearAvatars.Location = new System.Drawing.Point(126, 248);
             this.lnkClearAvatars.Name = "lnkClearAvatars";
             this.lnkClearAvatars.Size = new System.Drawing.Size(109, 20);
-            this.lnkClearAvatars.TabIndex = 6;
+            this.lnkClearAvatars.TabIndex = 7;
             this.lnkClearAvatars.Text = "Clear Avatar Cache";
             this.lnkClearAvatars.Click += new System.EventHandler(this.lnkClearAvatars_Click);
             // 
             // chkGPS
             // 
             this.chkGPS.ForeColor = System.Drawing.Color.LightGray;
-            this.chkGPS.Location = new System.Drawing.Point(1, 154);
+            this.chkGPS.Location = new System.Drawing.Point(1, 134);
             this.chkGPS.Name = "chkGPS";
             this.chkGPS.Size = new System.Drawing.Size(235, 20);
-            this.chkGPS.TabIndex = 10;
+            this.chkGPS.TabIndex = 5;
             this.chkGPS.Text = "Use GPS";
             // 
             // SettingsForm
@@ -198,11 +186,10 @@ namespace PockeTwit
             this.Controls.Add(this.chkTimestamps);
             this.Controls.Add(this.lnkAccounts);
             this.Controls.Add(this.chkReplyImages);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTweets);
             this.Controls.Add(this.txtMaxTweets);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.chkBeep);
-            this.Controls.Add(this.lblError);
             this.Controls.Add(this.chkVersion);
             this.Menu = this.mainMenu1;
             this.Name = "SettingsForm";
@@ -213,7 +200,6 @@ namespace PockeTwit
 
         #endregion
 
-        private System.Windows.Forms.Label lblError;
         private System.Windows.Forms.LinkLabel lnkAccounts;
         private System.Windows.Forms.CheckBox chkTimestamps;
         private System.Windows.Forms.LinkLabel lnkClearAvatars;
