e71f7ae33c738e0353f316ca8579224a62caacea
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-08-30 15:01:29 +0000

    Crash reporter allows you to choose account to send from and will synchronize it.

--- trunk/PockeTwit/Program.cs
+++ trunk/PockeTwit/Program.cs
@@ -10,15 +10,7 @@ namespace PockeTwit
 
 		#region Methods (2) 
 
-        static void SendError(string e)
-        {
-            Microsoft.WindowsMobile.PocketOutlook.EmailMessage m = new Microsoft.WindowsMobile.PocketOutlook.EmailMessage();
-            m.BodyText = e;
-            m.To.Add(new Microsoft.WindowsMobile.PocketOutlook.Recipient("pocketwitdev@gmail.com"));
-            m.Subject = "Crash Report";
-            Microsoft.WindowsMobile.PocketOutlook.OutlookSession s = new Microsoft.WindowsMobile.PocketOutlook.OutlookSession();
-            s.EmailAccounts[0].Send(m);
-        }
+        
 		// Private Methods (2) 
 
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
@@ -52,13 +44,12 @@ namespace PockeTwit
             {
                 w.Write(b.ToString());
             }
+
+            ChooseAccount form = new ChooseAccount(b.ToString());
+            form.ShowDialog();
+
             DialogResult res;
-            res = MessageBox.Show("An unexpected error has occured.  May I send an email containing the crash report to the developer?", "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
-            if(res == DialogResult.Yes)
-            {
-                SendError(b.ToString());
-                MessageBox.Show("Please open Pocket Outlook and Send/Recieve to transmit the error report.", "Error Report");
-            }
+            
         }
         /// <summary>
         /// The main entry point for the application.
