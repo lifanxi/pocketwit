891f1dfade49a6cd2c02c3e56e8cf898541d3880
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-09-30 15:19:47 +0000

    Be careful with dev builds.

--- trunk/PockeTwit/UpdateChecker.cs
+++ trunk/PockeTwit/UpdateChecker.cs
@@ -14,8 +14,8 @@ namespace PockeTwit
 
 		#region Fields (4) 
 
-        public static double currentVersion = .41;
-        public static bool devBuild = false;
+        public static double currentVersion = .40;
+        public static bool devBuild = true;
 
         private string UpdateURL = "http://pocketwit.googlecode.com/svn/LatestRelease/Release.xml";
         private UpdateInfo WebVersion;
