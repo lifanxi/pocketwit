eec9cc60a605fb9f60c88d9e852cb84f41a9aed9
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-10-31 13:16:01 +0000

    Try to fix GPS freezes some more.

--- trunk/PockeTwit/LocationManager.cs
+++ trunk/PockeTwit/LocationManager.cs
@@ -10,13 +10,12 @@ namespace PockeTwit
         //Ummm, just in case :)
         //http://maps.google.com/staticmap?center=37.393891,-122.066517&markers=37.400465,-122.073003,red&path=rgba:0x0000FF80,weight:5|37.40489,-122.05261&zoom=13&size=500x300&key=ABQIAAAA-6_yG-9k0X8KgfnjWXnHpBRtiFeQlxP7WphYAvKAZsrpIYWXFxQBOiC3o3mV0Wc7L8i2JnuMvxLdRQ
         //private const GoogleAPIKey = "ABQIAAAA-6_yG-9k0X8KgfnjWXnHpBRtiFeQlxP7WphYAvKAZsrpIYWXFxQBOiC3o3mV0Wc7L8i2JnuMvxLdRQ";
-        
+
         public delegate void delLocationReady(string Location);
         public event delLocationReady LocationReady;
         private GPS.GpsPosition position = null;
-        private GPS.GpsDeviceState device = null;
-        private GPS.Gps gps = null;
-        
+        private GPS.Gps gps = new PockeTwit.GPS.Gps();
+
         public void GetGPS()
         {
             if (ClientSettings.UseGPS)
@@ -27,44 +26,46 @@ namespace PockeTwit
 
         public void StartGPS()
         {
-            gps = new PockeTwit.GPS.Gps();
-            gps.DeviceStateChanged += new PockeTwit.GPS.DeviceStateChangedEventHandler(gps_DeviceStateChanged);
             gps.LocationChanged += new PockeTwit.GPS.LocationChangedEventHandler(gps_LocationChanged);
-
-            gps.Open();
+            if (!gps.Opened)
+            {
+                gps.Open();
+            }
         }
         public void StopGPS()
         {
-            gps.DeviceStateChanged -= new PockeTwit.GPS.DeviceStateChangedEventHandler(gps_DeviceStateChanged);
             gps.LocationChanged -= new PockeTwit.GPS.LocationChangedEventHandler(gps_LocationChanged);
-            gps.Close();
+            if (gps.Opened)
+            {
+                gps.Close();
+            }
         }
 
         void gps_LocationChanged(object sender, PockeTwit.GPS.LocationChangedEventArgs args)
         {
-            if (args.Position == null) { return; }
-            try
+            if (gps.Opened)
             {
-                if (args.Position.LatitudeValid && args.Position.LongitudeValid)
+                try
                 {
-                    if (!Double.IsNaN(args.Position.Longitude) && !Double.IsNaN(args.Position.Latitude))
+                    if (args.Position == null) { return; }
+                    if (args.Position.LatitudeValid && args.Position.LongitudeValid)
                     {
-                        position = args.Position;
-                        if (LocationReady != null)
+                        if (!Double.IsNaN(args.Position.Longitude) && !Double.IsNaN(args.Position.Latitude))
                         {
-                            LocationReady(position.Latitude.ToString() + "," + position.Longitude.ToString());
+                            position = args.Position;
+                            if (LocationReady != null)
+                            {
+                                LocationReady(position.Latitude.ToString() + "," + position.Longitude.ToString());
+                            }
                         }
                     }
                 }
-            }
-            catch (DivideByZeroException)
-            {
-            }
-        }
+                catch (DivideByZeroException)
+                {
+                }
 
-        void gps_DeviceStateChanged(object sender, PockeTwit.GPS.DeviceStateChangedEventArgs args)
-        {
-            device = args.DeviceState;
+            }
+
         }
     }
 }
