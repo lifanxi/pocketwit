119cdcd4de3d0a15575ca899e1923c2fdc87ff3c
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-11-08 21:30:09 +0000

    Hack to prevent bad url loops.

--- trunk/PockeTwit/FingerUI/Notification.cs
+++ trunk/PockeTwit/FingerUI/Notification.cs
@@ -8,23 +8,59 @@ namespace FingerUI
     class NotificationPopup
     {
         public Font TextFont;
-        private int Visibility = 0;
+        private int AnimationStep = 0;
+        private bool Visibility = false;
+        private int MaxHeight = ClientSettings.TextSize + (ClientSettings.Margin * 2);
+        public KListControl parentControl;
+        private System.Windows.Forms.Timer animationTimer = new System.Windows.Forms.Timer();
+        public NotificationPopup()
+        {
+            animationTimer.Interval = ClientSettings.AnimationInterval;
+            animationTimer.Tick += new EventHandler(animationTimer_Tick);
+        }
 
+        void animationTimer_Tick(object sender, EventArgs e)
+        {
+            parentControl.Invalidate();
+        }
+        
+        public void ShowNotification()
+        {
+            animationTimer.Enabled = true;
+            Visibility = true;
+        }
 
+        public void HideNotification()
+        {
+            Visibility = false;
+        }
         public void DrawNotification(Graphics g, int Bottom, int Width)
         {
-            Width = Width - (ClientSettings.Margin*2);
-            int Left = ClientSettings.Margin;
-            using (Brush ForeBrush = new SolidBrush(ClientSettings.ForeColor))
+            if (Visibility)
             {
-                using (Pen p = new Pen(ClientSettings.ForeColor))
+                if (AnimationStep < MaxHeight) { AnimationStep++; }
+            }
+            else
+            {
+                if (AnimationStep > 0) { AnimationStep--; }
+                else { animationTimer.Enabled = false; }
+
+            }
+            if (AnimationStep > 0)
+            {
+                Width = Width - (ClientSettings.Margin * 2);
+                int Left = ClientSettings.Margin;
+                using (Brush ForeBrush = new SolidBrush(ClientSettings.ForeColor))
                 {
-                    
-                    Rectangle boxPos = new Rectangle(Left, Bottom - (ClientSettings.TextSize + (ClientSettings.Margin * 2)), Width, ClientSettings.TextSize + (ClientSettings.Margin * 2));
-                    Rectangle textPos = new Rectangle(Left+ClientSettings.Margin, Bottom - (ClientSettings.TextSize + ClientSettings.Margin), Width- ClientSettings.Margin, ClientSettings.TextSize + ClientSettings.Margin);
-                    Gradient.GradientFill.Fill(g, boxPos, ClientSettings.SelectedBackGradColor, ClientSettings.SelectedBackColor, Gradient.GradientFill.FillDirection.TopToBottom);
-                    g.DrawRectangle(p, boxPos);
-                    g.DrawString("Fetching", TextFont, ForeBrush, textPos);
+                    using (Pen p = new Pen(ClientSettings.ForeColor))
+                    {
+
+                        Rectangle boxPos = new Rectangle(Left, Bottom - AnimationStep, Width, ClientSettings.TextSize + (ClientSettings.Margin * 2));
+                        Rectangle textPos = new Rectangle(Left + ClientSettings.Margin, (Bottom - AnimationStep)+ClientSettings.Margin, Width - ClientSettings.Margin, ClientSettings.TextSize + ClientSettings.Margin);
+                        Gradient.GradientFill.Fill(g, boxPos, ClientSettings.SelectedBackGradColor, ClientSettings.SelectedBackColor, Gradient.GradientFill.FillDirection.TopToBottom);
+                        g.DrawRectangle(p, boxPos);
+                        g.DrawString("Fetching", TextFont, ForeBrush, textPos);
+                    }
                 }
             }
         }
