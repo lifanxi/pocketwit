b317d61222bb2422eb3dcc590cd19dbd309d5074
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-11-19 02:58:56 +0000

    v.52 ready

--- PockeTwit/UpdateChecker.cs
+++ PockeTwit/UpdateChecker.cs
@@ -14,7 +14,7 @@ namespace PockeTwit
 
 		#region Fields (4) 
 
-        public static double currentVersion = .51;
+        public static double currentVersion = .52;
         public static bool devBuild = true;
 
         private string UpdateURL = "http://pocketwit.googlecode.com/svn/LatestRelease/Release.xml";
