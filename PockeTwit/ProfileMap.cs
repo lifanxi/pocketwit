95fc7724a73135a3ce346200443c2c63ef9be361
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-12-13 01:14:33 +0000

    v.57

--- PockeTwit/ProfileMap.cs
+++ PockeTwit/ProfileMap.cs
@@ -31,13 +31,28 @@ namespace PockeTwit
 
         private void DisplayMap()
         {
-            this.pictureBox1.Image = Yedda.GoogleMaps.GetMap(_Location, 2, pictureBox1.Height, pictureBox1.Width);
+            try
+            {
+                this.pictureBox1.Image = Yedda.GoogleMaps.GetMap(_Location, 2, pictureBox1.Height, pictureBox1.Width);
+            }
+            catch
+            {
+                pictureBox1.Visible = false;
+                Label newLabel = new Label();
+                newLabel.Text = "Unable to fetch map.";
+                newLabel.Location = new Point(0, 0);
+                this.Controls.Add(newLabel);
+
+            }
         }
 
         private void menuItem1_Click(object sender, EventArgs e)
         {
             this.pictureBox1.Resize -= new EventHandler(pictureBox1_Resize);
-            this.pictureBox1.Image.Dispose();
+            if (this.pictureBox1.Image != null)
+            {
+                this.pictureBox1.Image.Dispose();
+            }
             this.Close();
         }
 
