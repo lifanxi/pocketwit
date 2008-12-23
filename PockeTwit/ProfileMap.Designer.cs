eee5d41134c06b743244e4ff2c3b03672947c94e
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-12-23 18:54:57 +0000

    Jump to location in the map

--- PockeTwit/ProfileMap.Designer.cs
+++ PockeTwit/ProfileMap.Designer.cs
@@ -34,9 +34,13 @@
             this.menuItem2 = new System.Windows.Forms.MenuItem();
             this.menuZoomOut = new System.Windows.Forms.MenuItem();
             this.menuZoomIn = new System.Windows.Forms.MenuItem();
-            this.myPictureBox = new System.Windows.Forms.PictureBox();
             this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItem5 = new System.Windows.Forms.MenuItem();
             this.menuItem4 = new System.Windows.Forms.MenuItem();
+            this.myPictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBoxJump = new System.Windows.Forms.PictureBox();
+            this.txtJump = new System.Windows.Forms.TextBox();
+            this.lblJump = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mainMenu1
@@ -54,6 +58,7 @@
             this.menuItem2.MenuItems.Add(this.menuZoomOut);
             this.menuItem2.MenuItems.Add(this.menuZoomIn);
             this.menuItem2.MenuItems.Add(this.menuItem3);
+            this.menuItem2.MenuItems.Add(this.menuItem5);
             this.menuItem2.MenuItems.Add(this.menuItem4);
             this.menuItem2.Text = "Actions";
             // 
@@ -67,6 +72,20 @@
             this.menuZoomIn.Text = "Zoom In";
             this.menuZoomIn.Click += new System.EventHandler(this.menuZoomIn_Click);
             // 
+            // menuItem3
+            // 
+            this.menuItem3.Text = "-";
+            // 
+            // menuItem5
+            // 
+            this.menuItem5.Text = "Jump To...";
+            this.menuItem5.Click += new System.EventHandler(this.menuItem5_Click);
+            // 
+            // menuItem4
+            // 
+            this.menuItem4.Text = "Search Here";
+            this.menuItem4.Click += new System.EventHandler(this.menuItem4_Click_1);
+            // 
             // myPictureBox
             // 
             this.myPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -74,14 +93,34 @@
             this.myPictureBox.Name = "myPictureBox";
             this.myPictureBox.Size = new System.Drawing.Size(240, 268);
             // 
-            // menuItem3
+            // pictureBoxJump
             // 
-            this.menuItem3.Text = "-";
+            this.pictureBoxJump.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxJump.Location = new System.Drawing.Point(21, 82);
+            this.pictureBoxJump.Name = "pictureBoxJump";
+            this.pictureBoxJump.Size = new System.Drawing.Size(198, 59);
+            this.pictureBoxJump.Visible = false;
             // 
-            // menuItem4
+            // txtJump
             // 
-            this.menuItem4.Text = "Search Here";
-            this.menuItem4.Click += new System.EventHandler(this.menuItem4_Click_1);
+            this.txtJump.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtJump.Location = new System.Drawing.Point(30, 110);
+            this.txtJump.Name = "txtJump";
+            this.txtJump.Size = new System.Drawing.Size(176, 21);
+            this.txtJump.TabIndex = 2;
+            this.txtJump.Visible = false;
+            this.txtJump.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtJump_KeyUp);
+            this.txtJump.LostFocus += new System.EventHandler(this.txtJump_LostFocus);
+            // 
+            // lblJump
+            // 
+            this.lblJump.Location = new System.Drawing.Point(30, 87);
+            this.lblJump.Name = "lblJump";
+            this.lblJump.Size = new System.Drawing.Size(100, 20);
+            this.lblJump.Text = "Jump To:";
+            this.lblJump.Visible = false;
             // 
             // ProfileMap
             // 
@@ -89,6 +128,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.lblJump);
+            this.Controls.Add(this.txtJump);
+            this.Controls.Add(this.pictureBoxJump);
             this.Controls.Add(this.myPictureBox);
             this.Menu = this.mainMenu1;
             this.Name = "ProfileMap";
@@ -106,5 +148,9 @@
         private System.Windows.Forms.MenuItem menuZoomIn;
         private System.Windows.Forms.MenuItem menuItem3;
         private System.Windows.Forms.MenuItem menuItem4;
+        private System.Windows.Forms.PictureBox pictureBoxJump;
+        private System.Windows.Forms.TextBox txtJump;
+        private System.Windows.Forms.Label lblJump;
+        private System.Windows.Forms.MenuItem menuItem5;
     }
 }
