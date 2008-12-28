854231e19f41e395fd7861111eef05ddeac21af5
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2008-12-28 15:04:17 +0000

    Try Lois's sound code.

--- PockeTwit/Sound.cs
+++ PockeTwit/Sound.cs
@@ -12,6 +12,7 @@ namespace PockeTwit
     {
         private byte[] m_soundBytes;
         private string m_fileName;
+        private const string m_waveDevice = "WAV1:";
 
         private enum Flags
         {
@@ -36,12 +37,55 @@ namespace PockeTwit
         const int POWER_FORCE = 4096;
         const int POWER_STATE_RESET = 0x00800000;
 
+        public enum CEDEVICE_POWER_STATE : int
+        {
+          PwrDeviceUnspecified = -1,
+          //Full On: full power,  full functionality
+          D0 = 0,
+          /// <summary>
+          /// Low Power On: fully functional at low power/performance
+          /// </summary>
+          D1 = 1,
+          /// <summary>
+          /// Standby: partially powered with automatic wake
+          /// </summary>
+          D2 = 2,
+          /// <summary>
+          /// Sleep: partially powered with device initiated wake
+          /// </summary>
+          D3 = 3,
+          /// <summary>
+          /// Off: unpowered
+          /// </summary>
+          D4 = 4,
+          PwrDeviceMaximum
+        }
+
+        public enum DevicePowerFlags
+        {
+          None = 0,
+          /// <summary>
+          /// Specifies the name of the device whose power should be maintained at or above the DeviceState level.
+          /// </summary>
+          POWER_NAME = 0x00000001,
+          /// <summary>
+          /// Indicates that the requirement should be enforced even during a system suspend.
+          /// </summary>
+          POWER_FORCE = 0x00001000,
+          POWER_DUMPDW = 0x00002000
+        }
 
         [DllImport("CoreDll.DLL", EntryPoint = "PlaySoundW", SetLastError = true)]
         private extern static int WCE_PlaySound(string szSound, IntPtr hMod, int flags);
 
         [DllImport("CoreDll.DLL", EntryPoint = "PlaySoundW", SetLastError = true)]
         private extern static int WCE_PlaySoundBytes(byte[] szSound, IntPtr hMod, int flags);
+
+        [DllImport("CoreDLL", SetLastError = true)]
+        public static extern IntPtr SetDevicePower(string pDevice, DevicePowerFlags DeviceFlags, CEDEVICE_POWER_STATE DevicePowerState);
+
+        [DllImport("CoreDLL")]
+        public static extern int GetDevicePower(string device, DevicePowerFlags flags, out CEDEVICE_POWER_STATE PowerState);
 
         /// <summary>
         /// Construct the Sound object to play sound data from the specified file.
@@ -66,14 +110,33 @@ namespace PockeTwit
         /// </summary>
         public void Play()
         {
-            // if a file name has been registered, call WCE_PlaySound,
-            //  otherwise call WCE_PlaySoundBytes
-            //SetSystemPowerState(null, POWER_STATE_ON, POWER_FORCE);
+          // if a file name has been registered, call WCE_PlaySound,
+          //  otherwise call WCE_PlaySoundBytes
+          //SetSystemPowerState(null, POWER_STATE_ON, POWER_FORCE);
 
-            if (m_fileName != null)
-                WCE_PlaySound(m_fileName, IntPtr.Zero, (int)(Flags.SND_FILENAME));
-            else
-                WCE_PlaySoundBytes(m_soundBytes, IntPtr.Zero, (int)(Flags.SND_MEMORY));
+          // Check to see if the wave device is powered before playing a sound
+          CEDEVICE_POWER_STATE CurrAudioState = CEDEVICE_POWER_STATE.PwrDeviceUnspecified;
+
+          GetDevicePower(m_waveDevice, DevicePowerFlags.POWER_NAME, out CurrAudioState);
+
+          // If not powered, power up the wave device
+          if (CurrAudioState != CEDEVICE_POWER_STATE.D0)
+          {
+            SetDevicePower(m_waveDevice, DevicePowerFlags.POWER_NAME | DevicePowerFlags.POWER_FORCE, CEDEVICE_POWER_STATE.D0);
+          }
+
+          if (m_fileName != null)
+            WCE_PlaySound(m_fileName, IntPtr.Zero, (int)(Flags.SND_FILENAME));
+          else
+            WCE_PlaySoundBytes(m_soundBytes, IntPtr.Zero, (int)(Flags.SND_MEMORY));
+
+
+          if (CurrAudioState != CEDEVICE_POWER_STATE.PwrDeviceUnspecified)
+          {
+            // Change the wave device power state back to what it was
+            SetDevicePower(m_waveDevice, DevicePowerFlags.POWER_NAME | DevicePowerFlags.POWER_FORCE, CurrAudioState);
+          }
+
         }
     }
 }
