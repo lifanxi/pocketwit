bca5321a2ec2be30ac254c056ef39610600adca5
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-01-12 23:45:56 +0000

    Checkin new settings screen being developed.

---
+++ trunk/PockeTwit/SettingsHandler/NotificationSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PockeTwit.SettingsHandler
+{
+    public partial class NotificationSettings : Form
+    {
+        public NotificationSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbNotificationType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NotificationHandler.NotificationInfo currentInfo;
+            switch (cmbNotificationType.Text)
+            {
+                case "Friends":
+                    currentInfo = NotificationHandler.Friends;
+                    break;
+                case "Messages":
+                    currentInfo = NotificationHandler.Messages;
+                    break;
+            }
+
+            
+        }
+    }
+}
