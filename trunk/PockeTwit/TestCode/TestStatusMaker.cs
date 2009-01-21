c0efc92169cb35e3224ef49e2988276e44e915d3
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-01-21 14:57:20 +0000

    Added a TestStatusMaker class to generate new statuses when needed.

---
+++ trunk/PockeTwit/TestCode/TestStatusMaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace PockeTwit.TestCode
+{
+    static class TestStatusMaker
+    {
+        public static Library.status[] GenerateTestStatuses(int NumberOfItems)
+        {
+            Library.status[] ret = new PockeTwit.Library.status[NumberOfItems];
+
+            for (int i = 0; i < NumberOfItems; i++)
+            {
+                DateTime t = DateTime.Now.Subtract(new TimeSpan(0, 0, NumberOfItems - i));
+                ret[i] = new PockeTwit.Library.status();
+                ret[i].created_at = t.ToString();
+                ret[i].text = "Test status " + i;
+                ret[i].user = new PockeTwit.Library.User();
+                ret[i].user.name = "Test user" + i;
+            }
+            return ret;
+        }
+    }
+}
