31d393dc8c4254554b543bd60a20acc3e96768df
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-03-03 13:02:19 +0000

    Integrate Roel's changes.

---
+++ PockeTwit/Yedda/IPictureService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Web;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yedda
+{
+    /// <summary>
+    /// Interface for multiple picture services
+    /// </summary>
+    public interface IPictureService
+    {
+        /// <summary>
+        /// Send a picture to a twitter picture framework
+        /// </summary>
+        /// <param name="postData">Postdata</param>
+        /// <returns>Returned URL from server</returns>
+        string PostPicture(PicturePostObject postData);
+
+        /// <summary>
+        /// Retrieve a picture from a picture service. 
+        /// </summary>
+        /// <param name="pictureURL">pictureURL</param>
+        /// <returns>Local path for downloaded picture.</returns>
+        string FetchPicture(string pictureURL);
+        
+    }
+}
