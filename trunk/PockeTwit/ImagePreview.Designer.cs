158b5a726225d9f1be64ea9318a33b72176cbac2
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-03-15 15:45:50 +0000

    Image preview form added.

---
+++ trunk/PockeTwit/ImagePreview.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace PockeTwit
+{
+    partial class ImagePreview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItem1);
+            this.mainMenu1.MenuItems.Add(this.menuItem2);
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Text = "Dismiss";
+            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 268);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Text = "Launch URL";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            // 
+            // ImagePreview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.pictureBox1);
+            this.Menu = this.mainMenu1;
+            this.Name = "ImagePreview";
+            this.Text = "ImagePreview";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+    }
+}
---
+++ trunk/PockeTwit/ImagePreview.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PockeTwit
+{
+    public partial class ImagePreview : Form
+    {
+        private Bitmap imageToShow;
+        private string fullURL;
+        public ImagePreview(string ImageToShow, string FullURL)
+        {
+            InitializeComponent();
+            PockeTwit.Themes.FormColors.SetColors(this);
+            if (ClientSettings.IsMaximized)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+
+            fullURL = FullURL;
+            imageToShow = new Bitmap(ImageToShow);
+            this.pictureBox1.Image = imageToShow;
+            
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+            imageToShow.Dispose();
+            this.Close();
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            imageToShow.Dispose();
+            base.OnClosed(e);
+            
+        }
+
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = fullURL;
+            p.StartInfo.UseShellExecute = true;
+            p.Start();
+            imageToShow.Dispose();
+            this.Close();
+        }
+    }
+}
