6f34eee9980389051d8ffea64f52cb117d03cdab
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-03-15 13:18:13 +0000

    Experiment with upping the UI thread priority to prevent background tasks from slowing it.

--- trunk/PockeTwit/TweetList.cs
+++ trunk/PockeTwit/TweetList.cs
@@ -80,6 +80,7 @@ namespace PockeTwit
         #region�Constructors�(1)�
         public TweetList(bool InBackGround)
         {
+            System.Threading.Thread.CurrentThread.Priority = System.Threading.ThreadPriority.AboveNormal;
             StartBackground = InBackGround;
             Microsoft.WindowsCE.Forms.MobileDevice.Hibernate += new EventHandler(MobileDevice_Hibernate);
             Program.StartUp = DateTime.Now;
