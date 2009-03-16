c4fb4f3c165c0cb1db95be93dd0b1fcc196a8743
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-03-16 16:06:28 +0000

    Addressbook control added to post form

--- PockeTwit/AddressBook.cs
+++ PockeTwit/AddressBook.cs
@@ -7,11 +7,25 @@ namespace PockeTwit
 {
     static class AddressBook
     {
+        private static string location = ClientSettings.AppPath + "\\AddressBook.xml";
         private static List<string> _Names = new List<string>();
+
+        static AddressBook()
+        {
+            Load();
+        }
 
         public static string[] GetList()
         {
             return _Names.ToArray();
+        }
+
+        public static string[] GetList(string startsWith)
+        {
+            return _Names.FindAll(delegate(string s)
+            {
+                return s.StartsWith(startsWith);
+            }).ToArray();
         }
 
         public static void AddName(string Name)
@@ -20,8 +34,52 @@ namespace PockeTwit
                 if (!_Names.Contains(Name))
                 {
                     _Names.Add(Name);
+                    _Names.Sort();
+                    Save();
                 }
             }
         }
+
+        private static void Load()
+        {
+            if (System.IO.File.Exists(location))
+            {
+                using (System.IO.StreamReader r = new System.IO.StreamReader(location))
+                {
+                    string Addresses = r.ReadToEnd();
+                    if (!string.IsNullOrEmpty(Addresses))
+                    {
+                        System.Xml.XmlDocument d = new System.Xml.XmlDocument();
+                        d.LoadXml(Addresses);
+
+                        System.Xml.XmlNodeList l = d.SelectNodes("//name");
+                        foreach (System.Xml.XmlNode n in l)
+                        {
+                            if (!_Names.Contains(n.InnerText))
+                            {
+                                _Names.Add(n.InnerText);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private static void Save()
+        {
+            System.Xml.XmlDocument d = new System.Xml.XmlDocument();
+            System.Xml.XmlElement root = d.CreateElement("usernames");
+            d.AppendChild(root);
+
+            foreach (string ID in _Names)
+            {
+                System.Xml.XmlElement idElement = d.CreateElement("name");
+                idElement.InnerText = ID;
+                root.AppendChild(idElement);
+            }
+
+            d.Save(location);
+        }
+
+        
     }
 }
