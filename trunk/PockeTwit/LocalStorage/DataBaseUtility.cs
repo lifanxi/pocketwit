699cf1498b969a5dd81224e880fb0d667887eee7
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-04-07 14:02:48 +0000

    Code cleanup on DatabaseUtility

--- trunk/PockeTwit/LocalStorage/DataBaseUtility.cs
+++ trunk/PockeTwit/LocalStorage/DataBaseUtility.cs
@@ -1,47 +1,56 @@
 ﻿using System;
-
 using System.Collections.Generic;
-using System.Text;
 using System.Data.SQLite;
+using System.IO;
+using PockeTwit;
+using PockeTwit.Library;
+using Yedda;
 
 namespace LocalStorage
 {
-    class DataBaseUtility
+    internal class DataBaseUtility
     {
-        
         #region SQL Constants
+
+        private const string SQLCountFromCache =
+            @"SELECT     COUNT(id) AS newItems
+                          FROM         statuses WHERE
+                          (timestamp > 
+                            (SELECT timestamp from statuses WHERE id=@id))";
+
+        private const string SQLFetchDirects = "(statuses.statustypes & 2)";
+
+        private const string SQLFetchFriends = "(statuses.statustypes == 0)";
+
         private const string SQLFetchFromCache =
-                        @"SELECT     statuses.id, statuses.fulltext, statuses.userid, statuses.[timestamp], 
+            @"SELECT     statuses.id, statuses.fulltext, statuses.userid, statuses.[timestamp], 
                                      statuses.in_reply_to_id, statuses.favorited, statuses.clientSource, 
                                      statuses.accountSummary, statuses.statustypes, users.screenname, 
                                      users.fullname, users.description, users.avatarURL, statuses.statustypes
                           FROM       statuses INNER JOIN users ON statuses.userid = users.id";
-        private const string SQLCountFromCache =
-                        @"SELECT     COUNT(id) AS newItems
-                          FROM         statuses WHERE
-                          (timestamp > 
-                            (SELECT timestamp from statuses WHERE id=@id))";
+
+        private const string SQLFetchReplies = "(statuses.statustypes & 1)";
+
+        private const string SQLFetchRepliesAndMessages = " statuses.statustypes & 1 OR statuses.statustypes & 2 ";
+
+        private const string SQLFetchSearches = "(statuses.statustypes & 4)";
+
         private const string SQLGetLastStatusID =
-                        @"SELECT    statuses.id
+            @"SELECT    statuses.id
                           FROM statuses 
                           WHERE statuses.accountSummary = @accountsummary";
 
         private const string SQLIgnoreGrouped =
             //@" INNER JOIN usersInGroups ON statuses.userid <> usersInGroups.userid ";
-                        @" AND ((SELECT COUNT(id) FROM usersInGroups WHERE usersInGroups.userid=statuses.userid ) = 0)  ";
-        
-        private const string SQLFetchFriends = "(statuses.statustypes == 0)";
-        private const string SQLFetchReplies = "(statuses.statustypes & 1)";
-        
-        private const string SQLFetchRepliesAndMessages = " statuses.statustypes & 1 OR statuses.statustypes & 2 ";
+            @" AND ((SELECT COUNT(id) FROM usersInGroups WHERE usersInGroups.userid=statuses.userid ) = 0)  ";
 
-        private const string SQLFetchDirects = "(statuses.statustypes & 2)";
-        private const string SQLFetchSearches = "(statuses.statustypes & 4)";
-        private const string SQLOrder = " ORDER BY statuses.[timestamp] DESC ";
         private const string SQLLimit = " LIMIT @count ";
+        private const string SQLOrder = " ORDER BY statuses.[timestamp] DESC ";
+
         #endregion
 
-        private static string DBPath = ClientSettings.AppPath + "\\LocalStorage\\LocalCache.db";
+        private const string DBVersion = "0006";
+        private static readonly string DBPath = ClientSettings.AppPath + "\\LocalStorage\\LocalCache.db";
 
         public static void CheckDBSchema()
         {
@@ -50,45 +59,53 @@ namespace LocalStorage
                 using (SQLiteConnection conn = GetConnection())
                 {
                     conn.Open();
-                    using (SQLiteCommand comm = new SQLiteCommand(conn))
+                    using (var comm = new SQLiteCommand(conn))
                     {
-                        
                         comm.CommandText = "SELECT value from DBProperties WHERE name='dbversion'";
-                        string versionNum = (string)comm.ExecuteScalar();
+                        var versionNum = (string) comm.ExecuteScalar();
 
-                        if (versionNum == DBVersion) { return; }
+                        if (versionNum == DBVersion)
+                        {
+                            return;
+                        }
                     }
                     conn.Close();
                 }
             }
-            catch { }
+            catch(SQLiteException)
+            {
+            }
 
             DeleteDB();
         }
+
         private static void DeleteDB()
         {
-            if (System.IO.File.Exists(DBPath))
+            if (File.Exists(DBPath))
             {
-                System.IO.File.Delete(DBPath);
+                File.Delete(DBPath);
             }
         }
-        
-        public static System.Data.SQLite.SQLiteConnection GetConnection()
+
+        public static SQLiteConnection GetConnection()
         {
-            if (!System.IO.File.Exists(DBPath)) { CreateDB(); }
+            if (!File.Exists(DBPath))
+            {
+                CreateDB();
+            }
             return new SQLiteConnection("Data Source=" + ClientSettings.AppPath + "\\LocalStorage\\LocalCache.db");
         }
 
         //Update this number if you change the schema of the database -- it'll
         // force the client to recreate it.
-        private const string DBVersion = "0006";
+
         public static void CreateDB()
         {
-            if (!System.IO.Directory.Exists(ClientSettings.AppPath + "\\LocalStorage"))
+            if (!Directory.Exists(ClientSettings.AppPath + "\\LocalStorage"))
             {
-                System.IO.Directory.CreateDirectory(ClientSettings.AppPath + "\\LocalStorage");
+                Directory.CreateDirectory(ClientSettings.AppPath + "\\LocalStorage");
             }
-            if(!System.IO.File.Exists(DBPath))
+            if (!File.Exists(DBPath))
             {
                 SQLiteConnection.CreateFile(DBPath);
             }
@@ -96,7 +113,7 @@ namespace LocalStorage
             {
                 using (SQLiteConnection conn = GetConnection())
                 {
-                    using (SQLiteCommand comm = new SQLiteCommand(conn))
+                    using (var comm = new SQLiteCommand(conn))
                     {
                         conn.Open();
                         SQLiteTransaction t = conn.BeginTransaction();
@@ -116,7 +133,7 @@ namespace LocalStorage
                             comm.ExecuteNonQuery();
                             comm.Parameters.Clear();
                         }
-                        catch(SQLiteException ex)
+                        catch (SQLiteException)
                         {
                         }
                         comm.CommandText =
@@ -164,53 +181,58 @@ namespace LocalStorage
                     }
                 }
             }
-            catch (Exception ex)
+            catch (Exception)
             {
             }
         }
 
-        public static List<PockeTwit.Library.status> GetList(PockeTwit.TimelineManagement.TimeLineType typeToGet, int Count)
+        public static List<status> GetList(TimelineManagement.TimeLineType typeToGet, int Count)
         {
             string Constraints = null;
-            if (typeToGet == PockeTwit.TimelineManagement.TimeLineType.Friends)
+            if (typeToGet == TimelineManagement.TimeLineType.Friends)
             {
                 Constraints = SQLIgnoreGrouped;
             }
             return GetList(typeToGet, Count, Constraints);
         }
-        public static List<PockeTwit.Library.status> GetList(PockeTwit.TimelineManagement.TimeLineType typeToGet, int Count, string Constraints)
+
+        public static List<status> GetList(TimelineManagement.TimeLineType typeToGet, int Count, string Constraints)
         {
-            List<PockeTwit.Library.status> cache = new List<PockeTwit.Library.status>();
-            using (System.Data.SQLite.SQLiteConnection conn = LocalStorage.DataBaseUtility.GetConnection())
+            var cache = new List<status>();
+            using (SQLiteConnection conn = GetConnection())
             {
                 string FetchQuery = SQLFetchFromCache;
-                
-                FetchQuery = FetchQuery + " WHERE " +AddTypeWhereClause(typeToGet) + Constraints + SQLOrder + SQLLimit;
-                
-                using (System.Data.SQLite.SQLiteCommand comm = new System.Data.SQLite.SQLiteCommand(FetchQuery, conn))
+
+                FetchQuery = FetchQuery + " WHERE " + AddTypeWhereClause(typeToGet) + Constraints + SQLOrder + SQLLimit;
+
+                using (var comm = new SQLiteCommand(FetchQuery, conn))
                 {
                     comm.Parameters.Add(new SQLiteParameter("@count", Count));
                     conn.Open();
-                    using (System.Data.SQLite.SQLiteDataReader r = comm.ExecuteReader())
+                    using (SQLiteDataReader r = comm.ExecuteReader())
                     {
                         while (r.Read())
                         {
-                            PockeTwit.Library.status newStat = new PockeTwit.Library.status();
-                            newStat.id = r.GetString(0);
-                            newStat.text = r.GetString(1);
-                            newStat.TypeofMessage = (PockeTwit.Library.StatusTypes)r.GetInt32(13);
-                            newStat.createdAt = r.GetDateTime(3);
-                            newStat.in_reply_to_status_id = r.GetString(4);
-                            newStat.favorited = r.GetString(5);
-                            newStat.source = r.GetString(6);
-                            newStat.AccountSummary = r.GetString(7);
+                            var newStat = new status
+                                              {
+                                                  id = r.GetString(0),
+                                                  text = r.GetString(1),
+                                                  TypeofMessage = ((StatusTypes) r.GetInt32(13)),
+                                                  createdAt = r.GetDateTime(3),
+                                                  in_reply_to_status_id = r.GetString(4),
+                                                  favorited = r.GetString(5),
+                                                  source = r.GetString(6),
+                                                  AccountSummary = r.GetString(7)
+                                              };
 
-                            PockeTwit.Library.User u = new PockeTwit.Library.User();
-                            u.id = r.GetString(2);
-                            u.screen_name = r.GetString(9);
-                            u.name = r.GetString(10);
-                            u.description = r.GetString(11);
-                            u.profile_image_url = r.GetString(12);
+                            var u = new User
+                                        {
+                                            id = r.GetString(2),
+                                            screen_name = r.GetString(9),
+                                            name = r.GetString(10),
+                                            description = r.GetString(11),
+                                            profile_image_url = r.GetString(12)
+                                        };
 
                             newStat.user = u;
                             cache.Add(newStat);
@@ -222,36 +244,34 @@ namespace LocalStorage
             return cache;
         }
 
-        private static string AddTypeWhereClause(PockeTwit.TimelineManagement.TimeLineType typeToGet)
+        private static string AddTypeWhereClause(TimelineManagement.TimeLineType typeToGet)
         {
             switch (typeToGet)
             {
-                case PockeTwit.TimelineManagement.TimeLineType.Friends:
+                case TimelineManagement.TimeLineType.Friends:
                     return SQLFetchFriends;
-                    break;
-                case PockeTwit.TimelineManagement.TimeLineType.Messages:
+                case TimelineManagement.TimeLineType.Messages:
                     return SQLFetchRepliesAndMessages;
-                    break;
             }
             return null;
         }
-        
-        public static int GetItemsNewerThan(PockeTwit.TimelineManagement.TimeLineType typeToGet, string ID, string Constraints)
+
+        public static int GetItemsNewerThan(TimelineManagement.TimeLineType typeToGet, string ID, string Constraints)
         {
-            if (ID == null) 
+            if (ID == null)
             {
                 return 0;
             }
-            using (System.Data.SQLite.SQLiteConnection conn = LocalStorage.DataBaseUtility.GetConnection())
+            using (SQLiteConnection conn = GetConnection())
             {
                 string FetchQuery = SQLCountFromCache;
                 FetchQuery = FetchQuery + " AND " + AddTypeWhereClause(typeToGet) + Constraints + SQLOrder;
-                using (System.Data.SQLite.SQLiteCommand comm = new System.Data.SQLite.SQLiteCommand(FetchQuery, conn))
+                using (var comm = new SQLiteCommand(FetchQuery, conn))
                 {
                     comm.Parameters.Add(new SQLiteParameter("@id", ID));
                     conn.Open();
-                    object o= comm.ExecuteScalar();
-                    return System.Convert.ToInt32(o);
+                    object o = comm.ExecuteScalar();
+                    return Convert.ToInt32(o);
                 }
             }
         }
@@ -269,17 +289,17 @@ namespace LocalStorage
                 conn.Open();
                 using (SQLiteTransaction t = conn.BeginTransaction())
                 {
-                    using (SQLiteCommand comm = new SQLiteCommand(conn))
+                    using (var comm = new SQLiteCommand(conn))
                     {
                         comm.CommandText = "DELETE FROM statuses WHERE timestamp<@SinceDay";
                         comm.Parameters.Add(new SQLiteParameter("@SinceDay", SinceDate));
-                        comm.ExecuteNonQuery();
+                        int results = comm.ExecuteNonQuery();
                         comm.Parameters.Clear();
 
-                        /*
-                        comm.CommandText = "DELETE FROM users";
-                        comm.ExecuteNonQuery();
-                         */
+                        comm.CommandText = @"DELETE FROM users WHERE id NOT IN (
+                                                SELECT DISTINCT userid FROM statuses
+                                                )";
+                        results = comm.ExecuteNonQuery();
                     }
                     t.Commit();
                 }
@@ -287,13 +307,13 @@ namespace LocalStorage
             }
         }
 
-        public static void SaveItems(List<PockeTwit.Library.status> TempLine)
+        public static void SaveItems(List<status> TempLine)
         {
-            using (System.Data.SQLite.SQLiteConnection conn = LocalStorage.DataBaseUtility.GetConnection())
+            using (SQLiteConnection conn = GetConnection())
             {
                 conn.Open();
-                System.Data.SQLite.SQLiteTransaction t = conn.BeginTransaction();
-                foreach (PockeTwit.Library.status status in TempLine)
+                SQLiteTransaction t = conn.BeginTransaction();
+                foreach (status status in TempLine)
                 {
                     status.Save(conn);
                 }
@@ -302,41 +322,41 @@ namespace LocalStorage
             }
         }
 
-        public static string GetLatestItem(Yedda.Twitter.Account account, PockeTwit.TimelineManagement.TimeLineType typeToGet)
+        public static string GetLatestItem(Twitter.Account account, TimelineManagement.TimeLineType typeToGet)
         {
             string FetchQuery = SQLGetLastStatusID;
             switch (typeToGet)
             {
-                case PockeTwit.TimelineManagement.TimeLineType.Friends:
+                case TimelineManagement.TimeLineType.Friends:
                     FetchQuery = FetchQuery + " AND " + SQLFetchFriends + SQLOrder + SQLLimit;
                     break;
-                case PockeTwit.TimelineManagement.TimeLineType.Replies:
+                case TimelineManagement.TimeLineType.Replies:
                     FetchQuery = FetchQuery + " AND " + SQLFetchReplies + SQLOrder + SQLLimit;
                     break;
-                case PockeTwit.TimelineManagement.TimeLineType.Direct:
+                case TimelineManagement.TimeLineType.Direct:
                     FetchQuery = FetchQuery + " AND " + SQLFetchDirects + SQLOrder + SQLLimit;
                     break;
-                case PockeTwit.TimelineManagement.TimeLineType.Messages:
+                case TimelineManagement.TimeLineType.Messages:
                     FetchQuery = FetchQuery + " AND " + SQLFetchRepliesAndMessages + SQLOrder + SQLLimit;
                     break;
             }
-                
+
             using (SQLiteConnection conn = GetConnection())
             {
-                using (SQLiteCommand comm = new SQLiteCommand(FetchQuery, conn))
+                using (var comm = new SQLiteCommand(FetchQuery, conn))
                 {
                     comm.Parameters.Add(new SQLiteParameter("@accountsummary", account.Summary));
                     comm.Parameters.Add(new SQLiteParameter("@count", 1));
 
                     conn.Open();
-                    return (string)comm.ExecuteScalar();
+                    return (string) comm.ExecuteScalar();
                 }
             }
         }
 
         public static void VacuumDB()
         {
-            using(SQLiteCommand comm = GetConnection().CreateCommand())
+            using (SQLiteCommand comm = GetConnection().CreateCommand())
             {
                 comm.Connection.Open();
                 comm.CommandText = "vacuum;";
