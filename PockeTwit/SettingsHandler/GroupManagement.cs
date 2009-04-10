3dd67b8112d7fecf8b646d0394bcad4f2345a0c7
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-04-10 19:46:06 +0000

    Add group management form (nothing implemented)

---
+++ PockeTwit/SettingsHandler/GroupManagement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PockeTwit.SettingsHandler
+{
+    public partial class GroupManagement : BaseSettingsForm
+    {
+        public GroupManagement()
+        {
+            InitializeComponent();
+            PockeTwit.Themes.FormColors.SetColors(this);
+            if (ClientSettings.IsMaximized)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            } 
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
