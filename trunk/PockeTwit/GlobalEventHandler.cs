dc0acda42aa3df7e59f59431157c29783739977a
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-04-13 18:32:31 +0000

    Cleanup

--- trunk/PockeTwit/GlobalEventHandler.cs
+++ trunk/PockeTwit/GlobalEventHandler.cs
@@ -6,8 +6,6 @@ using System.Runtime.InteropServices;
 
 namespace PockeTwit
 {
-    //Since imagebuffers are part of an account and there is no good way to bubble events to the UI,
-    //  this static class allows me to create the events globally and subscribe in the UI.
     static class GlobalEventHandler
     {
         // Fields
@@ -15,9 +13,6 @@ namespace PockeTwit
         public static bool MessagesUpdating = false;
 
         // Delegates (1) 
-
-        public delegate void ArtWasUpdated(string User);
-        public delegate void delAvatarHasChanged(string User, string NewURL);
         public delegate void delNoData(Yedda.Twitter.Account t, Yedda.Twitter.ActionType Action);
         public delegate void delTimelineIsFetching(TimelineManagement.TimeLineType TType);
         public delegate void delTimelineIsDone(TimelineManagement.TimeLineType TType);
@@ -26,8 +21,6 @@ namespace PockeTwit
 
         // Events (1) 
 
-        public static event ArtWasUpdated Updated;
-        public static event delAvatarHasChanged AvatarHasChanged;
         public static event delTimelineIsFetching TimeLineFetching;
         public static event delTimelineIsDone TimeLineDone;
         public static event delshowErrorMessage ShowErrorMessage = delegate { };
@@ -93,21 +86,6 @@ namespace PockeTwit
         public static void CallShowErrorMessage(string Message)
         {
             ShowErrorMessage(Message);
-        }
-
-        public static void CallArtWasUpdated(string User)
-        {
-            if (Updated != null)
-            {
-                Updated(User);
-            }
-        }
-        public static void CallAvatarHasChanged(string User, string NewURL)
-        {
-            if (AvatarHasChanged != null)
-            {
-                AvatarHasChanged(User, NewURL);
-            }
         }
 
         public static void NotifyTimeLineFetching(TimelineManagement.TimeLineType TType)
