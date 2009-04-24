57095e3c7dee04758c0e509b4de34fe5bd287c63
dirk.rettschlag <dirk.rettschlag@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-04-24 10:47:47 +0000

    Fixed font size in today plugin. Added double buffering for today plugin to avoid flicker. Fixed AutoRunInstall issues.

--- AutoRunInstall/AutoRunInstall.cpp
+++ AutoRunInstall/AutoRunInstall.cpp
@@ -34,12 +34,12 @@ codeINSTALL_EXIT Install_Exit(
 	_tcscat(szPath, _T("PockeTwit.exe"));
 
 	// Refresh TodayScreen to show plugin
-	// PostMessage(HWND_BROADCAST, WM_WININICHANGE, 0xF2, 0);
-
+	PostMessage(HWND_BROADCAST, WM_WININICHANGE, 0xF2, 0);
+	
 	// Start the application, and don't wait for it to exit
     if (!CreateProcess(szPath, NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL, &pi))
 	{
-		MessageBox(GetForegroundWindow(), szPath, L"failed", MB_OK);
+		MessageBox(GetForegroundWindow(), szPath, L"Failed to start PockeTwit", MB_OK);
 	    cie = codeINSTALL_EXIT_UNINSTALL;
 	}
 
@@ -54,23 +54,23 @@ codeUNINSTALL_INIT Uninstall_Init(
     )
 {
 	// Disable Today Plugin
-	//HKEY key;
-	//DWORD dwEnabled, dwRet;
-	//DWORD lpcbData = sizeof(dwEnabled);
-	//
-	//dwRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE,_T("\\Software\\Microsoft\\Today\\Items\\PockeTwit"),0,0,&key);
+	HKEY key;
+	DWORD dwEnabled, dwRet;
+	DWORD lpcbData = sizeof(dwEnabled);
+	
+	dwRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE,_T("\\Software\\Microsoft\\Today\\Items\\PockeTwit"),0,0,&key);
+	
+	dwEnabled = 0;
+	
+	dwRet = RegSetValueEx(key,_T("Enabled"),0,REG_DWORD,(LPBYTE)&dwEnabled,sizeof(DWORD));
+	
+	RegFlushKey(key);
 
-	//dwEnabled = 0;
-	//
-	//dwRet = RegSetValueEx(key,_T("Enabled"),0,REG_DWORD,(LPBYTE)&dwEnabled,sizeof(DWORD));
-	//
-	//RegFlushKey(key);
+	RegCloseKey(key);
 
-	//RegCloseKey(key);
+	PostMessage(HWND_BROADCAST, WM_WININICHANGE, 0xF2, 0);
 
-	//PostMessage(HWND_BROADCAST, WM_WININICHANGE, 0xF2, 0);
-
-	//Sleep(2000);
+	Sleep(1000);
 
     return codeUNINSTALL_INIT_CONTINUE;
 }
