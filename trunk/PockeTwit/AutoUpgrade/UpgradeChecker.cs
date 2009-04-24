83d48da49434dc0dc4a89065c44ef1f7790a5a98
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-04-24 16:08:14 +0000

    v.71 done.  Checkin devbuild.

--- trunk/PockeTwit/AutoUpgrade/UpgradeChecker.cs
+++ trunk/PockeTwit/AutoUpgrade/UpgradeChecker.cs
@@ -14,7 +14,7 @@ namespace PockeTwit
 
 		#region Fields (4) 
 
-        public static double currentVersion = .70;
+        public static double currentVersion = .71;
         public static bool devBuild = true;
 
         private string UpgradeURL = "http://pocketwit.googlecode.com/svn/LatestRelease/Release.xml";
