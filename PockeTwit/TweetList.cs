a9831a6b6576dbdd460a22c8dd1243a40090053a
jakestevenson <jakestevenson@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-07-25 23:42:20 +0000

    More fixes from Jochen's branch.

--- PockeTwit/TweetList.cs
+++ PockeTwit/TweetList.cs
@@ -886,7 +886,7 @@ namespace PockeTwit
                     ConversationMenuItem.Visible = true;
                 }
 
-                if (selectedItem.Tweet.user.screen_name == selectedItem.Tweet.Account.UserName)
+                if (ClientSettings.GetAcountForUser(selectedItem.Tweet.user.screen_name) != null)
                 {
                     DeleteStatusMenuItem.Visible = true;
                 }
