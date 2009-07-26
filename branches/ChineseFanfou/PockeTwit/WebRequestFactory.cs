1894d121fddaf8d466fd7c3344aea4ac71f7cc1b
lifanxi <lifanxi@d6224f36-dd52-0410-a68d-81b86b79798e>
2009-07-26 13:37:26 +0000

    [Feature] Add proxy server support for PockeTwit. (Add missing file) The proxy settings will be kept in configuration file. New items are "ProxyServer" and "ProxyPort". The UI changed is the "other settings" page.

---
+++ branches/ChineseFanfou/PockeTwit/WebRequestFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace PockeTwit
+{
+    class WebRequestFactory
+    {
+        public static HttpWebRequest CreateHttpRequest(string url)
+        {
+            return CreateHttpRequest(new Uri(url));
+        }
+
+        public static HttpWebRequest CreateHttpRequest(Uri uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            if (!string.IsNullOrEmpty(ClientSettings.ProxyServer))
+            {
+                WebProxy proxy = new WebProxy(ClientSettings.ProxyServer, ClientSettings.ProxyPort);
+                proxy.BypassProxyOnLocal = true;
+                proxy.Credentials = CredentialCache.DefaultCredentials;
+                request.Proxy = proxy;
+            }
+            return request;
+        }
+    }
+}
