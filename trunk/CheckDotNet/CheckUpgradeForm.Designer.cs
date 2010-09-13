f353fb91e000c070786e5efe4668f93d9b30e289
ashley.brown@gmail.com <ashley.brown@gmail.com@d6224f36-dd52-0410-a68d-81b86b79798e>
2010-09-13 23:28:35 +0000

    .Net CAB downloader/installer for automated deployment.

---
+++ trunk/CheckDotNet/CheckUpgradeForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CheckDotNet
+{
+    partial class CheckUpgradeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.progressDownload = new System.Windows.Forms.ProgressBar();
+            this.lblDownloading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItem1);
+            this.mainMenu1.MenuItems.Add(this.menuItem2);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 70);
+            this.label1.Text = "PockeTwit requires version 3.5 of the .NET Compact Framework. Would you like to d" +
+                "ownload and run the upgrade now?";
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Text = "Yes";
+            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Text = "No";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            // 
+            // progressDownload
+            // 
+            this.progressDownload.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressDownload.Location = new System.Drawing.Point(4, 110);
+            this.progressDownload.Name = "progressDownload";
+            this.progressDownload.Size = new System.Drawing.Size(233, 20);
+            this.progressDownload.Visible = false;
+            // 
+            // lblDownloading
+            // 
+            this.lblDownloading.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDownloading.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lblDownloading.Location = new System.Drawing.Point(4, 86);
+            this.lblDownloading.Name = "lblDownloading";
+            this.lblDownloading.Size = new System.Drawing.Size(233, 20);
+            this.lblDownloading.Text = "Downloading...";
+            this.lblDownloading.Visible = false;
+            // 
+            // CheckUpgradeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.progressDownload);
+            this.Controls.Add(this.lblDownloading);
+            this.Controls.Add(this.label1);
+            this.Menu = this.mainMenu1;
+            this.Name = "CheckUpgradeForm";
+            this.Text = ".NET Upgrader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.ProgressBar progressDownload;
+        private System.Windows.Forms.Label lblDownloading;
+    }
+}
+
---
+++ trunk/CheckDotNet/CheckUpgradeForm.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+namespace CheckDotNet
+{
+    public partial class CheckUpgradeForm : Form
+    {
+
+        private HttpWebRequest request;
+        private HttpWebResponse response;
+        private FileStream filestream;
+
+        private int pbVal, maxVal;
+
+        // Data buffer for stream operations
+        private byte[] dataBuffer;
+        private const int DataBlockSize = 65536;
+
+        public CheckUpgradeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+            menuItem1.Click -= menuItem1_Click;
+            menuItem1.Click += menuItem1_CancelClick;
+
+            PerformUpdate();
+        }
+
+        private void menuItem1_CancelClick(object sender, EventArgs e)
+        {
+            menuItem2.Enabled = true;
+            menuItem1.Click -= menuItem1_CancelClick;
+            menuItem1.Click += menuItem1_Click;
+            menuItem1.Text = "Yes";
+            if (request != null)
+            {
+                request.Abort();
+                request = null;
+                
+            }
+            menuItem2_Click(sender, e);
+        }
+
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("PockeTwit will not run without this upgrade. Please visit the PockeTwit website to upgrade manually.", "Cancel Upgrade", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+            Close();
+        }
+
+        private delegate void delNothing();
+        private void PerformUpdate()
+        {
+            if (InvokeRequired)
+            {
+                delNothing d = PerformUpdate;
+                Invoke(d);
+            }
+            else
+            {
+                pbVal = 0;
+                Directory.CreateDirectory(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase) + "\\Update");
+                request = (HttpWebRequest)HttpWebRequest.Create("http://pocketwit.googlecode.com/files/NETCFv35.wm.armv4i.cab");
+                request.BeginGetResponse((ResponseReceived), null);
+                //EnableMenu(false);
+
+                menuItem1.Text = "Cancel";
+                menuItem2.Enabled = false;
+
+                lblDownloading.Visible = true;
+                progressDownload.Visible = true;
+            }
+
+            Cursor.Current = Cursors.WaitCursor;
+        }
+
+        public void SetProgressMax(object sender, EventArgs e)
+        {
+            progressDownload.Maximum = maxVal;
+            Application.DoEvents();
+        }
+        public void UpdateProgressValue(object sender, EventArgs e)
+        {
+            progressDownload.Value = pbVal;
+            Application.DoEvents();
+        }
+
+        void ResponseReceived(IAsyncResult res)
+        {
+            try
+            {
+                response = (HttpWebResponse)request.EndGetResponse(res);
+                // Allocate data buffer
+                dataBuffer = new byte[DataBlockSize];
+                // Set up progrees bar
+                maxVal = (int)response.ContentLength;
+                progressDownload.Invoke(new EventHandler(SetProgressMax));
+
+                // Open file stream to save received data
+                filestream = new FileStream(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase) + "\\Update\\NetUpgrade.cab", FileMode.Create);
+                // Request the first chunk
+                response.GetResponseStream().BeginRead(dataBuffer, 0, DataBlockSize, new AsyncCallback(OnDataRead), this);
+            }
+            catch(Exception e)
+            {
+                Invoke(new EventHandler(Error));
+            }
+        }
+
+        public void Error(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("There was an error downloading the upgrade.  Would you like to try again?", "PockeTwit", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                PerformUpdate();
+            }
+            else
+            {
+                MessageBox.Show("You can download the upgrade manually from http://code.google.com/p/pocketwit/");
+                Close();
+            }
+        }
+
+        void OnDataRead(IAsyncResult res)
+        {
+            try
+            {
+                // How many bytes did we get this time
+                int nBytes = response.GetResponseStream().EndRead(res);
+                // Write buffer
+                filestream.Write(dataBuffer, 0, nBytes);
+                // Update progress bar using Invoke()
+                pbVal += nBytes;
+
+                progressDownload.Invoke(new EventHandler(UpdateProgressValue));
+
+                // Are we done yet?
+                if (nBytes > 0)
+                {
+                    // No, keep reading
+                    response.GetResponseStream().BeginRead(dataBuffer, 0, DataBlockSize, new AsyncCallback(OnDataRead), this);
+                }
+                else
+                {
+                    if (response.StatusCode != HttpStatusCode.Unused)
+                    {
+                        // Yes, perform cleanup and update UI.
+                        filestream.Close();
+                        filestream = null;
+                        DoneDownloading();
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                Invoke(new EventHandler(Error));
+            }
+            catch (Exception ex)
+            {
+                Invoke(new EventHandler(Error));
+            }
+        }
+
+        void DoneDownloading()
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase) + "\\Update\\NetUpgrade.cab";
+            p.StartInfo.UseShellExecute = true;
+            p.Start();
+            Application.Exit();
+        }
+
+    }
+}
