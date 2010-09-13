ff4d2d86a220f381d59c64e0a0f0c06a5e464655
ashley.brown@gmail.com <ashley.brown@gmail.com@d6224f36-dd52-0410-a68d-81b86b79798e>
2010-09-13 23:28:35 +0000

    .Net CAB downloader/installer for automated deployment.

---
+++ CheckDotNet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CheckDotNet
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [MTAThread]
+        static void Main()
+        {
+            if(Environment.Version.Major < 3 || Environment.Version.Major == 3 && Environment.Version.Minor < 5)
+                Application.Run(new CheckUpgradeForm());
+            //System.Diagnostics.Process.Start("PockeTwit.exe", "");
+        }
+    }
+}
