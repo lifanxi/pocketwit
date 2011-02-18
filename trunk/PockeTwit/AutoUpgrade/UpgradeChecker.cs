6fb7d0c8d55015eb538ef300e482b176843df622
roelvandenbrand <roelvandenbrand@d6224f36-dd52-0410-a68d-81b86b79798e>
2011-02-18 21:37:58 +0000

    Start with version 87

--- trunk/PockeTwit/AutoUpgrade/UpgradeChecker.cs
+++ trunk/PockeTwit/AutoUpgrade/UpgradeChecker.cs
@@ -14,7 +14,7 @@ namespace PockeTwit
 
 		#region Fields (4) 
 
-        public static double currentVersion = .86;
+        public static double currentVersion = .87;
         public static bool devBuild = true;
         public static bool isBeta = false;
 
